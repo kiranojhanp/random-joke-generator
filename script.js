750ce1b6985fe27aeb9a8e9c063d33df2050d517
Kiran <kiranojha1226@gmail.com>
2021-01-14 22:51:16 +0545

    Bugfix

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const quoteContainer = document.getElementById("joke-container");
-const quoteText = document.getElementById("joke");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-joke");
+const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
 
 // Show Loading
