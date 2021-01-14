e81c22ec63bea193160fe2bd14e2abc638419a51
Kiran <kiranojha1226@gmail.com>
2021-01-14 22:42:59 +0545

    Final version

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const quoteContainer = document.getElementById("quote-container");
-const quoteText = document.getElementById("quote");
+const quoteContainer = document.getElementById("joke-container");
+const quoteText = document.getElementById("joke");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
+const newQuoteBtn = document.getElementById("new-joke");
 const loader = document.getElementById("loader");
 
 // Show Loading
